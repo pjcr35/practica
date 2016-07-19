c45bc284acadeaebed891a393cd8b99fbf614eae
pedro <pjcr35@hotmail.com>
2016-07-19 10:41:19 -0400

    3ra Subida

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
+	has_many :pruebadb
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718224946) do
+ActiveRecord::Schema.define(version: 20160719123450) do
 
   create_table "comments", force: :cascade do |t|
-    t.integer  "Post_id"
+    t.integer  "post_id"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
   end
 
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: :cascade do |t|
@@ -32,6 +33,15 @@ ActiveRecord::Schema.define(version: 20160718224946) do
   end
 
   add_index "posts", ["User_id"], name: "index_posts_on_User_id"
+
+  create_table "pruebadbs", force: :cascade do |t|
+    t.integer  "post_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "pruebadbs", ["post_id"], name: "index_pruebadbs_on_post_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "firs_name"
